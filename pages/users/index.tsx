8770b110130d81484b5e277e8f0941cef3505a6d
albamolera <albamolerachacon@gmail.com>
2022-06-09 19:52:51 +0200

    Implementación de búsqueda de usuarios por nombre

--- pages/users/index.tsx
+++ pages/users/index.tsx
@@ -37,13 +37,16 @@ function Users({ users }: any) {
 // This function gets called at build time on server-side.
 // It won't be called on client-side, so you can even do
 // direct database queries.
-export async function getStaticProps(context: any) {
+export async function getServerSideProps(context: any) {
   // Call an external API endpoint to get posts.
   // You can use any data fetching library
 
   console.log(context);
-  const query = context.query.query;
-  const res = await fetch("https://dummyjson.com/users/search?q=" + query);
+  const query = context.query.query || "";
+
+  const url = "https://dummyjson.com/users/search?q=" + query;
+
+  const res = await fetch(url);
   const data = await res.json();
 
   // By returning { props: { posts } }, the Blog component
