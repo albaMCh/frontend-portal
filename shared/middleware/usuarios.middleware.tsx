85f68b2e24f8f588511926b0a3cb614c3590113f
albamolera <albamolerachacon@gmail.com>
2022-06-10 18:29:20 +0200

    implementación de middleware de usuarios

--- shared/middleware/usuarios.middleware.tsx
+++ shared/middleware/usuarios.middleware.tsx
@@ -1,3 +1,41 @@
-export const getUsuarios = async () => {
-    
-}
+const API_URL = "https://dummyjson.com";
+const ENDPOINT = "users";
+
+export const getUsuarios = async ({ query }: { query?: string } = {}) => {
+  let isError = false;
+  try {
+    const url = `${API_URL}/${ENDPOINT}${query ? "/search?q=" + query : ""}`;
+    const res = await fetch(url);
+    const data = await res.json();
+
+    return {
+      data: data.users,
+      isError,
+    };
+  } catch (e) {
+    isError = true;
+    return {
+      data: [],
+      isError,
+    };
+  }
+};
+
+export const getUsuario = async (id: string) => {
+  let isError = false;
+  try {
+    let response = await fetch(`${API_URL}/${ENDPOINT}/${id}`);
+    const user = await response.json();
+
+    return {
+      data: user,
+      isError,
+    };
+  } catch (e) {
+    isError = true;
+    return {
+      data: null,
+      isError,
+    };
+  }
+};
